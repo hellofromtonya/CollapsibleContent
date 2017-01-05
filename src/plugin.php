88bc2f74cf8a9e0c9c582aae090e45a3b38db256
hellofromtonya <hellofromtonya@uptechlabs.io>
2017-01-04 20:53:37 -0600

    completed plugin - part 1

--- src/plugin.php
+++ src/plugin.php
@@ -1,10 +1,49 @@
 <?php
 /**
- * Description
+ * Plugin Handler
  *
- * @package     ${NAMESPACE}
+ * @package     KnowTheCode\CollapsibleContent
  * @since       1.0.0
  * @author      hellofromTonya
  * @link        https://KnowTheCode.io
  * @license     GNU-2.0+
  */
+namespace KnowTheCode\CollapsibleContent;
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+/**
+ * Enqueue the plugin assets (scripts and styles).
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function enqueue_assets() {
+	wp_enqueue_style('dashicons');
+
+	wp_enqueue_script(
+		'collapsible-content-plugin-script',
+		COLLAPSIBLE_CONTENT_URL . 'assets/dist/js/jquery.plugin.js',
+		array('jquery'),
+		'1.0.0',
+		true
+	);
+}
+
+/**
+ * Autoload plugin files.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function autoload() {
+	$files = array(
+		'shortcode/shortcodes.php',
+	);
+
+	foreach( $files as $file ) {
+		include( __DIR__ . '/' . $file );
+	}
+}
+autoload();
