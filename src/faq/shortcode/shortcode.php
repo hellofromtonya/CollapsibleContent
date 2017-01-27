ffa6109a7ed12b8c4172c50dcf893c1bbbf66678
hellofromtonya <hellofromtonya@uptechlabs.io>
2017-01-26 19:56:19 -0600

    topic + archive template

---
+++ src/faq/shortcode/shortcode.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Description
+ *
+ * @package     KnowTheCode\Module\FAQ\Shortcode
+ * @since       1.0.0
+ * @author      hellofromTonya
+ * @link        https://KnowTheCode.io
+ * @license     GNU-2.0+
+ */
+namespace KnowTheCode\Module\FAQ\Shortcode;
